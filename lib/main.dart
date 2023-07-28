9f30fba3b8164cd581886a93669afaa2821022fc
Günseli Ünsal <gunseliunsal@hotmail.com>
2023-07-28 16:54:47 +0300

    added images

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:modern_login_ui/pages/login_page.dart';
 
 void main() {
   runApp(const MyApp());
@@ -10,13 +11,11 @@ class MyApp extends StatelessWidget {
   // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      title: 'Modern Login UI',
-      theme: ThemeData(
-        colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
-        useMaterial3: true,
-      ),
-      home: const MyHomePage(title: 'Flutter Demo Home Page'),
-    );
+    return const MaterialApp(
+      debugShowCheckedModeBanner: false,
+      home: LoginPage(),
+    )
+      
+    ;
   }
 }
