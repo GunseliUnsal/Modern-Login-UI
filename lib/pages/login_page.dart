495bd77a82396e713b8d062c261e85f8e0835315
Günseli Ünsal <gunseliunsal@hotmail.com>
2023-07-28 17:29:23 +0300

    added username and password textfields

--- lib/pages/login_page.dart
+++ lib/pages/login_page.dart
@@ -1,10 +1,94 @@
 import 'package:flutter/material.dart';
+import 'package:modern_login_ui/components/my_textfield.dart';
 
 class LoginPage extends StatelessWidget {
-  const LoginPage({super.key});
+  LoginPage({super.key});
+
+  // Text editing controllers
+  final usernameController = TextEditingController();
+  final passwordController = TextEditingController();
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold();
+    return Scaffold(
+        backgroundColor: Colors.grey[300],
+        body: SafeArea(
+          child: Center(
+            child: Column(
+              children: [
+                const SizedBox(
+                  height: 50,
+                ),
+                //* logo
+                const Icon(
+                  Icons.lock,
+                  size: 100,
+                ),
+                const SizedBox(
+                  height: 50,
+                ),
+                //* welcome back, you've been missed!
+                Text(
+                  'Welcome back you\'ve been missed!',
+                  style: TextStyle(
+                    color: Colors.grey[700],
+                    fontSize: 16,
+                  ),
+                ),
+
+                const SizedBox(
+                  height: 50,
+                ),
+
+                //* username textfield
+                MyTextField(
+                  controller: usernameController,
+                  hintText: "Username",
+                  obscureText: false,
+                ),
+
+                const SizedBox(
+                  height: 10,
+                ),
+                //*password textfield
+                MyTextField(
+                  controller: passwordController,
+                  hintText: "Password",
+                  obscureText: true,
+                ),
+
+                const SizedBox(
+                  height: 10,
+                ),
+                //*forgot password?
+                Padding(
+                  padding: const EdgeInsets.symmetric(horizontal: 25.0),
+                  child: Row(
+                    mainAxisAlignment: MainAxisAlignment.end,
+                    children: [
+                      Text(
+                        "Forgot Password?",
+                        style: TextStyle(
+                          color: Colors.grey[600],
+                        ),
+                      ),
+                    ],
+                  ),
+                ),
+
+                const SizedBox(
+                  height: 25,
+                ),
+                //*sign in button
+
+                //* or continue with
+
+                //*google + apple sign in buttons
+
+                //* not a member? register now
+              ],
+            ),
+          ),
+        ));
   }
 }
